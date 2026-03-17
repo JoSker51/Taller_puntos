cb8422c7dc21b5b954e67d28f44fe715feff648f
Joske <112127990+JoSker51@users.noreply.github.com>
2026-03-16 21:54:29 -0500

    Add files via upload

---
+++ build/CMakeFiles/taller.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/taller.dir/src/filtros.cpp.o"
+  "CMakeFiles/taller.dir/src/filtros.cpp.o.d"
+  "CMakeFiles/taller.dir/src/main.cpp.o"
+  "CMakeFiles/taller.dir/src/main.cpp.o.d"
+  "taller"
+  "taller.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/taller.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
